750a04fc4f09fc9438ec7c9b4c6cc7339734b189
greendog21 <69486663+greendog21@users.noreply.github.com>
2021-06-21 14:08:45 -0500

    still working on sign in page

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,22 +1,61 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 const SignIn = () => {
-  const onFoucs = () => {};
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const inputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setUsername(event.target.value);
+  };
+
+  const onFocusHandler = () => {};
+
+  const submitHandler = (event) => {
+    if (username.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+  };
 
   return (
     <View style={styles.mainContainer}>
-      <Text style={styles.topTitle}>WELCOME!</Text>
       <Text style={styles.title}>FOOD LINES</Text>
       <Text style={styles.subTitle}>Sign in to access your console</Text>
-
-      <TextInput
-        styles={styles.userInput}
-        placeholder="Username"
-        keyboardType="text"
-        autoCompleteType="username"
-        onFocus
-      />
+      <View>
+        <Text style={(styles.label, { color: !isValid ? "red" : "#525252" })}>
+          Username
+        </Text>
+        <TextInput
+          styles={
+            (styles.userInput, { borderColor: !isValid ? "red" : "black" })
+          }
+          placeholder="Username"
+          autoCompleteType="username"
+          maxLength={20}
+          onFocus={onFocusHandler}
+          onChange={inputChangeHandler}
+        />
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          styles={styles.userInput}
+          placeholder="Password"
+          autoCompleteType="username"
+          maxLength={20}
+          onFocus={onFocusHandler}
+        />
+        <Button
+          style={styles.submitButton}
+          title="Sign In"
+          backgroundColor="#000000"
+          color="#fffffff"
+          onPress={submitHandler}
+        />
+      </View>
     </View>
   );
 };
@@ -24,7 +63,14 @@ const SignIn = () => {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    padding: 24,
+    padding: 30,
+    marginTop: 40,
+    backgroundColor: "#ffffff",
+  },
+  userInputContainer: {
+    flex: 1,
+    marginTop: 20,
+    marginBottom: 20,
     backgroundColor: "#ffffff",
   },
   topTitle: {
@@ -45,10 +91,28 @@ const styles = StyleSheet.create({
     textAlign: "left",
     fontSize: 15,
   },
+  label: {
+    flex: 1,
+    color: "#525252",
+    textAlign: "left",
+    fontSize: 15,
+  },
   userInput: {
-    borderWidth: 1,
+    flex: 2,
+    borderWidth: 2,
+    padding: 10,
     borderColor: "#8c8c8c",
     borderRadius: 5,
+    borderBottomColor: "#8c8c8c",
+    borderRightColor: "#8c8c8c",
+    borderTopColor: "#8c8c8c",
+    borderLeftColor: "#8c8c8c",
+    fontSize: 12,
+  },
+  submitButton: {
+    flex: 1,
+    padding: 10,
+    fontSize: 12,
   },
 });
 
