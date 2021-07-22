cf27147a45a8039facdeb13c54c28343f5ab1f10
Lint Action <lint-action@foodlines.com>
2021-07-22 23:00:37 +0000

    Fix code style issues with Prettier

--- src/Pages/ResetPassword.tsx
+++ src/Pages/ResetPassword.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, Platform, StatusBar, TextInput, TouchableOpacity, Text, Dimensions} from 'react-native'
+import {
+  View,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+} from 'react-native'
 
 // icons
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import Feather from 'react-native-vector-icons/Feather';
+import Feather from 'react-native-vector-icons/Feather'
 
 //Routes
 import { MainRoutes } from '../Navigators/routes'
@@ -26,7 +35,6 @@ import {
   StyledFormArea,
   ForgetPassword,
   ErrorMsg,
-
 } from '../Components/styles'
 
 // KeyboardAvoidingWrapper
@@ -37,19 +45,18 @@ import { useReduxDispatch } from '../Redux'
 import { login } from '../Redux/slices/user'
 
 // Colors
-const { brand, darkLight, primary, white, black, grey, greyLight} = Colors
+const { brand, darkLight, primary, white, black, grey, greyLight } = Colors
 
 //Gradient
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient'
 
 //Animations
-import * as Animatable from 'react-native-animatable';
-import { alignSelf } from 'styled-system';
+import * as Animatable from 'react-native-animatable'
+import { alignSelf } from 'styled-system'
 
 //Screen Dimensions
-const {height} = Dimensions.get("screen");
-const height_logo = height * 0.18;
-
+const { height } = Dimensions.get('screen')
+const height_logo = height * 0.18
 
 const SignIn = ({ navigation }): React.ReactElement => {
   const dispatch = useReduxDispatch()
@@ -57,7 +64,7 @@ const SignIn = ({ navigation }): React.ReactElement => {
 
   const [data, setData] = useState({
     email: '',
-    password:'',
+    password: '',
     confirmPassword: '',
     bday: '',
     fullName: '',
@@ -66,14 +73,14 @@ const SignIn = ({ navigation }): React.ReactElement => {
     secureTextEntry: true,
     confirmSecureTextEntry: true,
     isValidEmail: true,
-    isValidName: true, 
+    isValidName: true,
     isValidPassword: true,
     isValidConfirmPassword: true,
   })
 
   //Handlers
 
-  const onEmailChangeHandler = (val) =>{
+  const onEmailChangeHandler = (val) => {
     if (val.length >= 6 && val.includes('@') && val.includes('.')) {
       setData({
         ...data,
@@ -87,49 +94,42 @@ const SignIn = ({ navigation }): React.ReactElement => {
         email: val,
         check_email: false,
         isValidEmail: false,
-
       })
     }
   }
 
-  const onPasswordChangeHandler = (val) =>{
-    if (val.trim().length >=8) {
+  const onPasswordChangeHandler = (val) => {
+    if (val.trim().length >= 8) {
       setData({
         ...data,
         password: val,
-        isValidPassword: true
+        isValidPassword: true,
       })
     } else {
       setData({
         ...data,
         password: val,
-        isValidPassword: false
-
-
+        isValidPassword: false,
       })
     }
-
   }
 
-  const onConfirmPasswordChangeHandler = (val) =>{
+  const onConfirmPasswordChangeHandler = (val) => {
     if (val.trim() === data.password) {
       setData({
         ...data,
         confirmPassword: val,
-        isValidConfirmPassword: true
+        isValidConfirmPassword: true,
       })
     } else {
       setData({
         ...data,
         confirmPassword: val,
-        isValidConfirmPassword: false
-
-
+        isValidConfirmPassword: false,
       })
     }
-}
+  }
 
-  
   const onNameChangeHandler = (val) => {
     if (val.trim().length > 1 && val.includes(' ')) {
       setData({
@@ -137,7 +137,6 @@ const SignIn = ({ navigation }): React.ReactElement => {
         fullName: val,
         check_name: true,
         isValidName: true,
-
       })
     } else {
       setData({
@@ -146,104 +145,106 @@ const SignIn = ({ navigation }): React.ReactElement => {
         check_name: false,
         isValidName: false,
       })
-      
-      
-
     }
-    
   }
 
   const updateSecureTextEntry = () => {
     setData({
       ...data,
-      secureTextEntry: !data.secureTextEntry
+      secureTextEntry: !data.secureTextEntry,
     })
   }
 
   const updateConfirmSecureTextEntry = () => {
     setData({
       ...data,
-      confirmSecureTextEntry: !data.confirmSecureTextEntry
+      confirmSecureTextEntry: !data.confirmSecureTextEntry,
     })
   }
 
-
-  const onSubmitHandler = async() => { //Prob Not Correct but whatever
+  const onSubmitHandler = async () => {
+    //Prob Not Correct but whatever
     navigation.navigate(MainRoutes.NewUserWelcome)
-
   }
   return (
     <KeyboardAvoidingWrapper>
       <StyledContainerFullScreen>
         <StatusBar barStyle="light-content" />
         <View style={styles.header}>
-          <Animatable.Image 
+          <Animatable.Image
             style={styles.logo}
-            source={require('../Assets/delta.png')} 
+            source={require('../Assets/delta.png')}
             resizeMode="cover"
             animation="bounceIn"
-            duration={1500}/>
-          <Animatable.View animation="fadeInLeft" duration={500}> 
+            duration={1500}
+          />
+          <Animatable.View animation="fadeInLeft" duration={500}>
             <SignInTextHeader>Step 2:</SignInTextHeader>
             <Text style={styles.text_headerDetail}>Choose New Password</Text>
-          </Animatable.View>   
+          </Animatable.View>
         </View>
 
         <Animatable.View style={styles.footer} animation="fadeInUpBig">
           <StyledFormArea>
-            <SignInTextFooter style={{marginTop: 0}}>New Password</SignInTextFooter>
+            <SignInTextFooter style={{ marginTop: 0 }}>
+              New Password
+            </SignInTextFooter>
             <SignInAction>
               <FontAwesome name="lock" color={black} size={20} />
-              <TextInput 
-                style={styles.textInput} 
-                placeholder="* * * * * * * * *" 
-                autoCapitalize="none" 
-                secureTextEntry={data.secureTextEntry ? true: false} 
-                onChangeText={(val)=>onPasswordChangeHandler(val)}
+              <TextInput
+                style={styles.textInput}
+                placeholder="* * * * * * * * *"
+                autoCapitalize="none"
+                secureTextEntry={data.secureTextEntry ? true : false}
+                onChangeText={(val) => onPasswordChangeHandler(val)}
               />
               <TouchableOpacity onPress={updateSecureTextEntry}>
-                {data.secureTextEntry ?
-                <Feather name="eye-off" color={grey} size={20} />
-                : 
-                <Feather name="eye" color={grey} size={20} />
-                }
+                {data.secureTextEntry ? (
+                  <Feather name="eye-off" color={grey} size={20} />
+                ) : (
+                  <Feather name="eye" color={grey} size={20} />
+                )}
               </TouchableOpacity>
             </SignInAction>
-            {data.isValidPassword ?  null :
-            <Animatable.View animation="fadeInLeft" duration={500}>
-              <ErrorMsg>Password must be at least 8 characters long.</ErrorMsg>
-            </Animatable.View>
-            }
+            {data.isValidPassword ? null : (
+              <Animatable.View animation="fadeInLeft" duration={500}>
+                <ErrorMsg>
+                  Password must be at least 8 characters long.
+                </ErrorMsg>
+              </Animatable.View>
+            )}
 
-            <SignInTextFooter style={{marginTop: 25}}>Confirm Password</SignInTextFooter>
+            <SignInTextFooter style={{ marginTop: 25 }}>
+              Confirm Password
+            </SignInTextFooter>
             <SignInAction>
               <FontAwesome name="lock" color={black} size={20} />
-              <TextInput 
-                style={styles.textInput} 
-                placeholder="* * * * * * * * *" 
-                autoCapitalize="none" 
-                secureTextEntry={data.confirmSecureTextEntry ? true: false} 
-                onChangeText={(val)=>onConfirmPasswordChangeHandler(val)}
+              <TextInput
+                style={styles.textInput}
+                placeholder="* * * * * * * * *"
+                autoCapitalize="none"
+                secureTextEntry={data.confirmSecureTextEntry ? true : false}
+                onChangeText={(val) => onConfirmPasswordChangeHandler(val)}
               />
               <TouchableOpacity onPress={updateConfirmSecureTextEntry}>
-                {data.confirmSecureTextEntry ?
-                <Feather name="eye-off" color={grey} size={20} />
-                : 
-                <Feather name="eye" color={grey} size={20} />
-                }
+                {data.confirmSecureTextEntry ? (
+                  <Feather name="eye-off" color={grey} size={20} />
+                ) : (
+                  <Feather name="eye" color={grey} size={20} />
+                )}
               </TouchableOpacity>
             </SignInAction>
-            {data.isValidConfirmPassword ?  null :
-            <Animatable.View animation="fadeInLeft" duration={500}>
-              <ErrorMsg>Must match password.</ErrorMsg>
-            </Animatable.View>
-            }
+            {data.isValidConfirmPassword ? null : (
+              <Animatable.View animation="fadeInLeft" duration={500}>
+                <ErrorMsg>Must match password.</ErrorMsg>
+              </Animatable.View>
+            )}
 
             <SignInButton onPress={onSubmitHandler}>
               <LinearGradient
                 colors={['#FFA07A', '#FF6347']}
                 style={styles.signIn}
-                >
+              >
                 <SignInTextSign>Next</SignInTextSign>
               </LinearGradient>
             </SignInButton>
@@ -251,21 +252,16 @@ const SignIn = ({ navigation }): React.ReactElement => {
         </Animatable.View>
       </StyledContainerFullScreen>
     </KeyboardAvoidingWrapper>
-
-
-  
   )
 }
 
 export default SignIn
 
-
 const styles = StyleSheet.create({
-
   text_headerDetail: {
-      color: white,
-      fontWeight: 'bold',
-      fontSize: 20
+    color: white,
+    fontWeight: 'bold',
+    fontSize: 20,
   },
   logo: {
     width: height_logo,
@@ -275,67 +271,66 @@ const styles = StyleSheet.create({
   },
 
   container: {
-    flex: 1, 
-    backgroundColor: '#FF6347'
+    flex: 1,
+    backgroundColor: '#FF6347',
   },
   header: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      paddingHorizontal: 20,
-      paddingBottom: 50
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
   },
   footer: {
-      flex: Platform.OS === 'ios' ? 2 : 3,
-      backgroundColor: white,
-      borderTopLeftRadius: 30,
-      borderTopRightRadius: 30,
-      paddingHorizontal: 20,
-      paddingVertical: 30
+    flex: Platform.OS === 'ios' ? 2 : 3,
+    backgroundColor: white,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
   text_header: {
-      color: white,
-      fontWeight: 'bold',
-      fontSize: 30
+    color: white,
+    fontWeight: 'bold',
+    fontSize: 30,
   },
   text_footer: {
-      color: black,
-      fontSize: 18
+    color: black,
+    fontSize: 18,
   },
   action: {
-      flexDirection: 'row',
-      marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: grey,
-      paddingBottom: 5
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: grey,
+    paddingBottom: 5,
   },
   textInput: {
-      flex: 1,
-      marginTop: Platform.OS === 'ios' ? 0 : -12,
-      paddingLeft: 10,
-      color: black,
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: black,
   },
   button: {
-      alignItems: 'center',
-      marginTop: 50
+    alignItems: 'center',
+    marginTop: 50,
   },
   signIn: {
-      width: '100%',
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 10
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
   },
   textSign: {
-      fontSize: 18,
-      fontWeight: 'bold'
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   textPrivate: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      marginTop: 20
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
   },
   color_textPrivate: {
-      color: grey
-  }
-  
-});
+    color: grey,
+  },
+})
