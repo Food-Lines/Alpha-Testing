ad7fa8c0a1ef263777b0b349602f743b4f41b147
greendog21 <69486663+greendog21@users.noreply.github.com>
2021-07-22 15:26:03 -0500

    unhijacked

--- src/Navigators/MainNavigation.tsx
+++ src/Navigators/MainNavigation.tsx
@@ -20,26 +20,30 @@ import Confirmation from '../Pages/Confirmation'
 import NewUserWelcome from '../Pages/NewUserWelcome'
 import FoodAccounts from '../Pages/FoodAccounts'
 
-import {
-  getAuth
-} from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 import Firebase from '../../config/Firebase.js'
-
-
 
 //Screens Main
 import NavBar from '../Pages/NavBar'
 
-const { primary, tertiary, white, black} = Colors
-import  userSlice  from '../Redux/slices/user'
+const { primary, tertiary, white, black } = Colors
+import userSlice from '../Redux/slices/user'
 
 const loggedIn = (): boolean => {
   const user = getAuth(Firebase).currentUser
   const reduxUser = useReduxSelector(selectUser)
   if (user && reduxUser.uid === user.uid) return true
-  else if (user && reduxUser.uid !== user.uid) {userSlice.actions.setUser(user); return true}
-  else if (!user && reduxUser.uid) {userSlice.actions.setUser({email: null as string, fullName: null as string, uid: null as string}); return false}
-  else return false
+  else if (user && reduxUser.uid !== user.uid) {
+    userSlice.actions.setUser(user)
+    return true
+  } else if (!user && reduxUser.uid) {
+    userSlice.actions.setUser({
+      email: null as string,
+      fullName: null as string,
+      uid: null as string,
+    })
+    return false
+  } else return false
 }
 
 const MainNavigation = (): React.ReactElement => {
@@ -61,43 +65,57 @@ const MainNavigation = (): React.ReactElement => {
         }}
         initialRouteName={MainRoutes.NavBar}
       >
-        <>
+        {/* <>
           <MainStack.Screen name={MainRoutes.NavBar} component={NavBar} />
-        </>
-        {/* {!user.uid ? (
+        </> */}
+        {!user.uid ? (
           <>
-            <MainStack.Screen name={MainRoutes.SplashScreen} component={SplashScreen} />
-            <MainStack.Screen name={MainRoutes.SignIn} component={SignIn} options={{headerShown: false}} />
-            <MainStack.Screen name={MainRoutes.SignUp} component={SignUp} options={{headerShown: false}} /> 
-            <MainStack.Screen name={MainRoutes.NewUserWelcome} component={NewUserWelcome} options={{headerShown: true}} />
-            <MainStack.Screen name={MainRoutes.FoodAccounts} component={FoodAccounts} options={{headerShown: true}} /> 
-            
-            
+            <MainStack.Screen
+              name={MainRoutes.SplashScreen}
+              component={SplashScreen}
+            />
+            <MainStack.Screen
+              name={MainRoutes.SignIn}
+              component={SignIn}
+              options={{ headerShown: false }}
+            />
+            <MainStack.Screen
+              name={MainRoutes.SignUp}
+              component={SignUp}
+              options={{ headerShown: false }}
+            />
+            <MainStack.Screen
+              name={MainRoutes.NewUserWelcome}
+              component={NewUserWelcome}
+              options={{ headerShown: true }}
+            />
+            <MainStack.Screen
+              name={MainRoutes.FoodAccounts}
+              component={FoodAccounts}
+              options={{ headerShown: true }}
+            />
+
             <MainStack.Screen
               name={MainRoutes.ConfirmEmail}
               component={ConfirmEmail}
-              options={{headerTintColor: white}}
+              options={{ headerTintColor: white }}
             />
             <MainStack.Screen name={MainRoutes.OTP} component={OTP} />
             <MainStack.Screen
               name={MainRoutes.ResetPassword}
               component={ResetPassword}
-              options={{headerShown: true}}
-
+              options={{ headerShown: true }}
             />
             <MainStack.Screen
               name={MainRoutes.Confirmation}
               component={Confirmation}
             />
-             
-             
           </>
         ) : (
           <>
-          <MainStack.Screen name={MainRoutes.NavBar} component={NavBar} />
-  
+            <MainStack.Screen name={MainRoutes.NavBar} component={NavBar} />
           </>
-        )} */}
+        )}
       </MainStack.Navigator>
     </NavigationContainer>
   )
